e36f1c24e08f8b61bc7dbb4c2b46dd821e3e67fa
reeveress <zuhraa470@gmail.com>
2018-02-18 19:19:42 -0800

    Added model definition of all other tables

--- index.js
+++ index.js
@@ -42,16 +42,115 @@ var Genres = connection.define('genres', {
 		timestamps: false,
 		freezeTableName: true
 	}
-
-	
 );
+
+var Films = connection.define('films', {
+	id : {
+				type: Sequelize.INTEGER,
+				primaryKey:true
+	},
+    	title: {
+				type: Sequelize.STRING
+	},
+    	release_date: {
+				type : Sequelize.STRING
+	},
+    	tagline: {
+				type: Sequelize.TEXT 
+	},
+    	revenue: {
+				type:Sequelize.INTEGER
+	},
+    	budget: {
+				type: Sequelize.INTEGER
+	},
+    	runtime: {
+				type:Sequelize.INTEGER
+	},	
+	original_language: {
+				type: Sequelize.STRING
+	},
+	status: {
+				type: Sequelize.STRING
+	},
+	genre_id: {
+				type:Sequelize.INTEGER
+	}
+	},{
+				timestamps: false,
+				freezeTableName: true
+	}
+);
+
+var Artist_films = connection.define('artist_films', {
+	id: {
+			type: Sequelize.INTEGER,
+			primaryKey:true
+	},
+	credit_type: {
+		     	type: Sequelize.STRING
+	},
+	role: {
+			type: Sequelize.STRING
+	},
+    	description: {
+		     	type: Sequelize.STRING
+	},
+	artist_id: {
+		   	type: Sequelize.INTEGER
+	},
+    	film_id: {
+		 	type: Sequelize.INTEGER
+	}
+	},{
+			timestamps: false,
+			freezeTableName: true
+	}	
+);
+
+var Artists = connection.define('artists', {
+	id: {
+			type: Sequelize.INTEGER,
+			primaryKey:true
+	},
+	name: {
+		     	type: Sequelize.STRING
+	},
+	birthday: {
+			type: Sequelize.STRING
+	},
+    	deathday: {
+		     	type: Sequelize.STRING
+	},
+	gender: {
+		   	type: Sequelize.INTEGER
+	},
+    	place_of_birth: {
+		 	type: Sequelize.STRING
+	}
+	},{
+			timestamps: false,
+			freezeTableName: true
+	}	
+);
+
+
+
 
 connection.sync().then( function () {
 	Genres.findAll().then( function ( genres ) {
-	console.log("Number of records in a talbe:" + genres.length);
-	
+	console.log("Number of records in genre talbe:" + genres.length);
 	});
-
+	
+	Films.findAll().then( function ( films ) {
+	console.log("Number of records in genre talbe:" + films.length);
+	});
+	Artist_films.findAll().then( function ( artist_films ) {
+	console.log("Number of records in genre talbe:" + artist_films.length);
+	});
+	Artists.findAll().then( function ( artists ) {
+	console.log("Number of records in genre talbe:" + artists.length);
+	});
 });
 
 
